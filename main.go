ac19cb8ae118d1610bc2a2d7022f247477d899f5
kaisa316 <490788241@qq.com>
2019-11-12 08:32:58 +0800

    gin init

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/fvbock/endless"
 )
 
 func main() {
@@ -38,8 +37,45 @@ func main() {
 		c.String(200, "hello world")
 		// fmt.Println("hello world")
 	})
-	//router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-	endless.
+	groupStudy(router)
+
+	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+
+	/*
+		srv := &http.Server{
+			Addr:    ":8080",
+			Handler: router,
+		}
+
+		go func() {
+			// service connections
+			if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+				log.Fatalf("listen: %s\n", err)
+			}
+		}()
+
+		// Wait for interrupt signal to gracefully shutdown the server with
+		// a timeout of 5 seconds.
+		quit := make(chan os.Signal)
+		// kill (no param) default send syscanll.SIGTERM
+		// kill -2 is syscall.SIGINT
+		// kill -9 is syscall. SIGKILL but can"t be catch, so don't need add it
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		<-quit
+		log.Println("Shutdown Server ...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Fatal("Server Shutdown:", err)
+		}
+		// catching ctx.Done(). timeout of 5 seconds.
+		select {
+		case <-ctx.Done():
+			log.Println("timeout of 5 seconds.")
+		}
+		log.Println("Server exiting")
+	*/
 }
 
 //middleware
@@ -61,5 +97,14 @@ func Logger() gin.HandlerFunc {
 		// access the status we are sending
 		status := c.Writer.Status()
 		log.Println(status)
+	}
+}
+
+func groupStudy(router *gin.Engine) {
+	v1 := router.Group("v1")
+	{
+		v1.GET("hello", func(c *gin.Context) {
+			c.String(http.StatusOK, "hello world")
+		})
 	}
 }
